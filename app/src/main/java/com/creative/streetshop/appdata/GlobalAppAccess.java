c597ff676df3e534d145ed885018c408ddf48a80
jubayer <mohammadjubayer999@gmail.com>
2018-04-24 00:03:20 +0600

    Initial commit

---
+++ app/src/main/java/com/creative/streetshop/appdata/GlobalAppAccess.java
@@ -0,0 +1,45 @@
+package com.creative.streetshop.appdata;
+
+
+
+public class GlobalAppAccess {
+
+
+    public static final String APP_NAME = "StreetShop";
+    public static String BaseUrl = "http://198.204.244.250:8084/";
+    //public static String BaseUrl = "https://b5e99a4d.ngrok.io/bgb/";
+    public static final String URL_GET_TIMES =  BaseUrl + "getTimes.jsp";
+    public static final String URL_INSERT_TIME = BaseUrl +  "insertTime.jsp";
+    public static final String URL_DELETE_TIME = BaseUrl +  "deleteTime.jsp";
+
+
+    public static final  int SUCCESS = 1;
+    public static  final  int ERROR = 0;
+
+
+
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_GLOBAL = "global";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+
+    public static final String KEY_CALL_FROM = "call_from";
+    public static final String KEY_NOTIFICATION_ID = "notification_id";
+
+    public static final String TAG_ALARM_RECEIVER = "alarm_receiver";
+
+    public static final String[] reminder_time_options = {"Select a time", "Before time expires", "15 mins before",
+            "30 mins before","1 hour before"};
+
+    public static final String[] rideshares_options = {"Uber", "Lyft", "Uber or Lyft"};
+
+    public static final String[] seats_options = {"4 Seats", "5 Seats"};
+
+}
---
+++ app/src/main/java/com/creative/streetshop/sharedprefs/PrefManager.java
@@ -0,0 +1,60 @@
+package com.creative.streetshop.sharedprefs;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.creative.streetshop.BuildConfig;
+import com.google.gson.Gson;
+
+
+/**
+ * Created by jubayer on 6/6/2017.
+ */
+
+
+public class PrefManager {
+    private static final String TAG = PrefManager.class.getSimpleName();
+
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    private static Gson GSON = new Gson();
+    // Sharedpref file name
+    private static final String PREF_NAME = BuildConfig.APPLICATION_ID;
+
+    private static final String KEY_NUM_OF_TIME_USER_SET_ALARM = "num_of_time_user_set_alarm";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+
+    }
+
+    public void setNumberOfTimeUserSetAlarm(int obj) {
+        editor = pref.edit();
+
+        editor.putInt(KEY_NUM_OF_TIME_USER_SET_ALARM, obj);
+
+        // commit changes
+        editor.commit();
+    }
+    public int getNumberOfTimeUserSetAlarm() {
+        return pref.getInt(KEY_NUM_OF_TIME_USER_SET_ALARM,0);
+    }
+
+
+
+
+
+}
